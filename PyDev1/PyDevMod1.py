c627657b6c901bc336f986f233f7f5ff9ce05043
lucian.stoicescu <lucian.stoicescu@localhost>
2020-01-16 17:02:38 -0500

    Randomize the way to find things

--- PyDev1/PyDevMod1.py
+++ PyDev1/PyDevMod1.py
@@ -4,26 +4,28 @@ Created on 5 Nov 2019
 @author: Lucian
 '''
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 #from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
-
+# import os
+# 
+# 
+# print(os.environ['PATH'])
 
 
 options = Options()
 options.headless = False
 
-driver=webdriver.Firefox(options=options)
+driver=webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.get("https://dev.thegrommet.com")
 
 
-
-
 assert "Grommet" in driver.title, "The title does not contain Grommet since it is" + driver.title
 sleep(3)
-driver.find_element_by_xpath('//*[contains(text(),"FitKicks")]').click()
+driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/div[2]/div').find_elements_by_tag_name('a')[1].click()
+#driver.find_element_by_xpath('//*[contains(text(),"FitKicks")]').click()
 #driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/div[2]/div/a[2]/div/div[1]').click()
 #     WebDriverWait(self.driver, 100).until(
 #             lambda driver: driver.find_element_by_name(self.locator))
