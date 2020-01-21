a2b960b61fb579d4d4d0d851d60c2847eab79e12
lucian.stoicescu <lucian.stoicescu@localhost>
2020-01-21 11:18:20 -0500

    Functional  version - fixed headless and proper element ids

--- PyDev1/PyDevMod1.py
+++ PyDev1/PyDevMod1.py
@@ -1,36 +1,31 @@
 '''
-Created on 5 Nov 2019
-
 @author: Lucian
 '''
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-#from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
-# import os
-# 
-# 
-# print(os.environ['PATH'])
 
 
+#Set headless, window size (It's 800X600 by default) and an implicit wait - set up proper wait later
 options = Options()
-options.headless = False
-
+options.headless = True
+options.add_argument("--window-size=1600,1200")
 driver=webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
+
+#Go to the main site and check that you're there
 driver.get("https://dev.thegrommet.com")
-
-
 assert "Grommet" in driver.title, "The title does not contain Grommet since it is" + driver.title
-sleep(3)
-driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/div[2]/div').find_elements_by_tag_name('a')[1].click()
-#driver.find_element_by_xpath('//*[contains(text(),"FitKicks")]').click()
-#driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/div[2]/div/a[2]/div/div[1]').click()
-#     WebDriverWait(self.driver, 100).until(
-#             lambda driver: driver.find_element_by_name(self.locator))
-#     element2 = self.driver.find_element_by_name(self.locator)
-
-    #wait = WebDriverWait(driver,10)
-sleep(3)
+#Click the second item in the recent discoveries group
+driver.find_element_by_xpath('//div[@class="tile-container-3 tile-container-2-mobile"]').find_elements_by_tag_name('a')[1].click()
+#Click the second item in the maker page
+driver.find_element_by_xpath('//div[@class="tile-container-4 tile-spacing-4 tile-container-2-mobile tile-spacing-2-mobile "]').find_elements_by_tag_name('a')[1].click()
+#Select 3 items
+driver.find_element_by_xpath("//select/option[text()='3']").click()
+#Add them to cart
+driver.find_element_by_xpath('//button[@formaction="cart.add"]').click()
+#Check add-to-cart toast text
+assert "3 items in your cart" in driver.find_element_by_xpath('//div[@class="flex-row flex-justify-center"]').text, "Text found: " + driver.find_element_by_xpath('//div[@class="flex-row flex-justify-center"]').text
+#Close browser
 driver.quit()
