4fb7a7eebf993c525dbeeaaa3669f25faac613c0
Lucian <Lucian@DESKTOP-63HJE43.cable.rcn.com>
2020-02-18 00:37:32 -0500

    Chrome fix should work - re-check safari

--- PyDev1/PyDevMod1.py
+++ PyDev1/PyDevMod1.py
@@ -9,19 +9,19 @@ from time import sleep
 
 #Set headless, window size (It's 800X600 by default) and an implicit wait - set up proper wait later
 options = Options()
-options.headless = False
+options.headless = True
 #options.add_argument("--window-size=1600,1200")
 # driver=webdriver.Safari()
 # driver.maximize_window()
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
-driver.maximize_window()
+driver.set_window_size(1920, 1080, 'current')
 
 #Go to the main site and check that you're there
 driver.get("https://dev.thegrommet.com")
 assert "Grommet" in driver.title, "The title does not contain Grommet since it is" + driver.title
 #Click the second item in the recent discoveries group
-driver.find_element_by_xpath('//div[@class="tile-container-3 tile-container-2-mobile"]').find_elements_by_tag_name('a')[0].click()
+driver.find_element_by_xpath('//div[@class="tile-container-3 tile-container-2-mobile"]').find_elements_by_tag_name('a')[1].click()
 #Click the second item in the maker page
 driver.find_element_by_xpath('//div[@class="p-t-l p-b-s p-t-m-phone p-b-0-phone"]').find_elements_by_tag_name('a')[1].click()
 #Select 3 items
@@ -29,7 +29,7 @@ driver.find_element_by_xpath("//select/option[text()='3']").click()
 #Add them to cart
 driver.find_element_by_xpath('//button[@formaction="cart.add"]').click()
 #Check add-to-cart toast text
-testEleme = driver.find_element_by_xpath('//*[@id="svelten4gMSNVq"]/div/div/div[1]/div[3]/div/div/div')
-assert "3 items in your cart" in testEleme.text
+testEleme = driver.find_element_by_xpath('//*[@id="svelteSMNED0YE"]/div/div/div[1]/div[3]/div/div/div/div/div')                                                
+assert "2 items in your cart" in testEleme.text, testEleme.text
 #Close browser
 driver.quit()
